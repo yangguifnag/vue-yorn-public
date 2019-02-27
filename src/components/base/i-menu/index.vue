945389f376aa8d2026be39df3fc1bba39b4a3a57
yangguifang <1782257089@QQ.COM>
2019-02-28 00:06:03 +0800

    update

---
+++ src/components/base/i-menu/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="yorn-contextmenu">
+		
+	</div>
+</template>
+<script>
+	
+	export default {
+		props :{
+			show : {
+				type : Boolean,
+				default : false
+			}
+		}
+	}
+</script>
+<style scoped>
+	.yorn-contextmenu{
+		width: 90px;
+		height: 90px;
+		position: absolute;
+	}
+</style>
